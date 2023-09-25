c513fbefeabedb72f6a2fa52c20f6a0225bfb54f
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-09-25 19:00:20 +0300

    Update api-handler.js

--- src/js/api-handler.js
+++ src/js/api-handler.js
@@ -11,10 +11,6 @@ export default class PixabayApiService {
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '39582157-9a04a9ecff1d4e24afa36bf7b';
 
-    //---видалити і не повертати---
-    // if (this.page === 14) {
-    //   return;
-    // }
     const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`;
     const result = axios.get(url);
 
